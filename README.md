9a89852ff8cb63de7eab92ae3b7150ee7b586bf1
Timothy Cheng <tim.cheng09@gmail.com>
2018-03-05 03:09:38 +0000

    beer bot can now log into sainsbury and search for beer * refactored beer searching into a separate file * added .env-example * updated readme.md

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # beerbot
+
+## Commands
+
+* search|find [beer]
+
+## Tech Stack
+
+* [Botkit](https://howdy.ai/botkit/) - Toolkit for creating bots
+* [Blue Bird](http://bluebirdjs.com/) - Feature-rich Promise library
+* [Fuse.js](http://fusejs.io/) - Lightweight fuzzy-search library
+
+## How to Run
+
+### Local Setup
+
+1. Sign up for <http://www.slack.com> and create a team if you don't have admin rights in your current one
+2. Create a bot integration and note down the API key
+3. Make a copy of `.env-example` in the root folder and name it `.env`
+4. Add the necessary environment variables to the file. See here for more details  <https://www.npmjs.com/package/dotenv>
+5. Run `npm start`
+6. Chat with your bot!
+
+### Heroku Setup
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+1. After performing the local setup and seeing that everything works, sign up for Heroku on the free plan with the button above
+2. Follow the tutorial for [Getting Started on Heroku with Node.js](https://devcenter.heroku.com/articles/getting-started-with-nodejs) if you don't know how Heroku works
+3. Create a new Heroku app from your project root
+4. Install <https://github.com/xavdid/heroku-config> and push your .env settings to heroku
+5. Run `heroku scale web=0 worker=1` to make sure the chatbot isn't required to bind to the provided Heroku port, otherwise Heroku will shut the app down thinking the app is not responsive since it didn't bind to the port.
+6. Push your app to the Heroku repo and tail the logs
+7. Chat with your cloud-enabled bot!
--- index.js
+++ index.js
@@ -1,8 +1,112 @@
-const sainsbury = require('./lib/sainsbury.js')
+require('dotenv').config();
 
-sainsbury.getBeers()
-	.then(pageArray => console.log(`pageArray=${JSON.stringify(pageArray)}`))
-	.catch(function (err) {
-	    // Crawling failed or Cheerio choked...
-	    console.error(err)
-	});
+if (!process.env.SLACK_TOKEN) {
+    console.log('Error: Specify slack bot token in .env (e.g. SLACK_TOKEN=abc)');
+    process.exit(1);
+}
+
+if (!process.env.USERNAME) {
+    console.log('Error: Specify your sainsbury username in .env (e.g. USERNAME=test@example.com)');
+    process.exit(1);
+}
+
+if (!process.env.PASSWORD) {
+    console.log('Error: Specify your sainsbury password in .env (e.g. PASSWORD=Password1)');
+    process.exit(1);
+}
+
+// requires
+const sainsbury = require('./lib/sainsbury.js')
+const Fuse = require('fuse.js');
+const Botkit = require('botkit');
+const Promise = require("bluebird");
+const _ = require('lodash');
+
+// Fuse options
+const DEFAULT_FUSE_OPTIONS = {
+    include: ["score"],
+    shouldSort: true,
+    threshold: 0.3,
+}
+const SEARCH_NAME_OPTIONS = Object.assign({
+	keys: ["name"]
+}, DEFAULT_FUSE_OPTIONS)
+
+
+const controller = Botkit.slackbot({
+	require_delivery: true,
+});
+
+var bot = controller.spawn({
+    token: process.env.SLACK_TOKEN
+}).startRTM();
+
+// Promisify bot methods
+Promise.promisifyAll(bot);
+
+function randResponse(responses) {
+    return responses[_.random(responses.length-1)];
+}
+
+function fatalError(message, error, customMessage) {
+    console.error("*******Fatal Error******* " + customMessage + "\n", error);
+    bot.reply(message, ":face_with_head_bandage: *My brain exploded:* _" + customMessage + "_");
+}
+
+function searchBeer(searchTerm) {
+	return sainsbury.getBeers()
+		
+		.then(allBeers => {
+			let fuse = new Fuse(allBeers, SEARCH_NAME_OPTIONS);
+	        return fuse.search(searchTerm);	
+	    })
+}
+
+function formatBeerResults(beers) {
+	let attachments = []
+	beers.forEach(beer => {
+		attachments.push({
+            "fallback": beer.name,
+            "title": beer.name,
+            "title_link": beer.url,
+            "thumb_url": beer.imageUrl,
+        })
+	})
+	return attachments
+}
+
+controller.hears('(?:search|find) (.+)',['direct_message', 'direct_mention'], function(bot, message) {
+    let searchTerm = message.match[1];
+    bot.reply(message, 'querying the intergalactic smart beer fridge *beep* *boop* *beep* *boop*...')
+    searchBeer(searchTerm)
+    	.then(beers => {
+    		console.log(`searchTerm=${searchTerm}, beers=${JSON.stringify(beers)}`)
+    		return beers
+    	})
+    	.then(beers => beers.slice(0, Math.min(beers.length, 20)))
+    	.then(beers => formatBeerResults(beers))
+    	.then(beerAttachments => {
+    		if (beerAttachments.length > 0) {
+	    		bot.reply(message, {
+		    		text: `Here are the top 20 results from Sainsbury!`,
+		    		attachments: beerAttachments
+	    		})
+    		} else {
+	    		bot.reply(message, 'Sorry little fella, I got a goose egg :egg: :man-shrugging:')
+    		}
+    	})
+    	.catch(e => fatalError(message, e, "Something weird happened when I tried searching..."));
+});
+
+controller.hears('\\?$','direct_message,direct_mention,mention', function(bot, message) {
+    bot.reply(message, randResponse([
+        'I don\'t like the tone of your question so I\'m not going to answer it',
+        'You dropped out for a second and I didn\'t catch that, can you repeat it?',
+        'You know, I asked my maker that the other day and he shut me down.',
+        ':sleeping::sleeping::sleeping:'
+    ]));
+});
+
+controller.hears('.+','direct_message,direct_mention,mention', function(bot, message) {
+    bot.reply(message, "I'll get back to you tomorrow");
+});
